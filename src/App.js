2acb3d08a77980e7160a314134585f2115f1b45d
pratikash <pratikashpanda.1234@gmail.com>
2023-05-28 13:20:05 +0530

    contact built

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Layout/Header";
 import Footer from "./components/Layout/Footer";
+import Contact from "./components/contact/Contact";
 import "./styles/app.scss";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
         <Footer/>
       </Router>
